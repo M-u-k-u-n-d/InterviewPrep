08626ed919ceb2ca6786c1ff55b60004f7bcd317
Mukund Tiwari <mukundtiwari251103@gmail.com>
2024-07-02 14:17:10 +0530

    first commit

---
+++ javascript/01.js
@@ -0,0 +1,47 @@
+// var vs let vs const
+// Scope - certain region of program where defined variable exists and can be recoginsed and beyond that cannot be recoginsed.
+
+/* 
+Global scope
+Block scope
+Functional scope
+*/
+/******** comment out below code */
+// let
+//  a = 5; // global scope
+
+// function func() {
+//     let b = 90; // functional scope
+// }
+// func();
+
+// {
+//     const y = 90; // block scope
+// }
+
+
+/*************** */
+
+// Shadowing 
+
+/******** comment out below code */
+
+// var a1 = 90;
+
+// function shadow(){
+//     a1 = 12;
+//     console.log(a1);
+    
+//     // let b = "Illegal shadowing";
+//     // var b = "rewrite";
+//     // console.log(b);
+
+// }
+// shadow();
+// console.log(a1);
+
+/************ */
+
+
+
+
---
+++ javascript/02.js
@@ -0,0 +1,14 @@
+Function.prototype.getAddress = function() {
+   console.log("hii");
+}
+
+const obj = {
+    name : "Mukund",
+    age: 26,
+    address : "Ballia"
+}    
+
+
+const val = {
+   x: 90};
+console.log(obj.prototype)
